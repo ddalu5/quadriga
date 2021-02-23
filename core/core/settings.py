46742f56e928c95a0736e0cebda3c0832729471c
Salah Osfor <osfor.salah@gmail.com>
2021-02-23 21:25:46 +0100

    Remove secret key from core settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yqnt@60w^za8k86&#tynkfk(y98*eo88v1xqt&&_#ine58bf76'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
